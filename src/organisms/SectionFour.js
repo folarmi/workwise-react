573f00eeaa935f5b44081b1ad9af801bd03160a8
folarmi <fola4ril@gmail.com>
2021-03-21 15:48:42 +0000

    finished landing page

--- src/organisms/SectionFour.js
+++ src/organisms/SectionFour.js
@@ -3,6 +3,7 @@ import { Aside } from "../molecules";
 import blog from "../assets/blog.png";
 import blogTwo from "../assets/blogTwo.png";
 import { SectionWrapper } from "../layout/SectionWrapper";
+import { Link } from "react-router-dom";
 
 const SectionFour = () => {
   return (
@@ -19,19 +20,46 @@ const SectionFour = () => {
           <div>
             <Aside title="From Our Blog" className="lg:w-48" />
           </div>
-          <div>
-            {/* <div className="">
-              <p className="text-black bg-white rounded-md">
-                Workwise raises $100m Series A funding from Salesforce.
-              </p>
-            </div> */}
-            <img
-              src={blog}
-              alt="blog"
-              className="rounded-sm my-4 mb-12 lg:w-full"
-            />
-            <img src={blogTwo} alt="blogTwo" className="rounded-sm lg:w-full" />
-          </div>
+
+          <section>
+            <div className="relative">
+              <div className="absolute top-4 left-4 p-4 ml-4 mt-4 bg-white rounded-md h-52 w-44">
+                <p className="text-black font-extrabold mb-12">
+                  Workwise raises $100m Series A funding from Salesforce.
+                </p>
+                <Link
+                  to="/blog"
+                  className="text-primary underline text-base font-semibold"
+                >
+                  Read full article
+                </Link>
+              </div>
+              <img
+                src={blog}
+                alt="blog"
+                className="rounded-sm my-4 mb-12 lg:w-full"
+              />
+            </div>
+
+            <div className="relative">
+              <div className="absolute top-4 left-4 p-4 ml-4 mt-4 bg-white rounded-md h-52 w-44">
+                <p className="text-black font-extrabold mb-12">
+                  Hermann Powe joins Workwise as new Head of Design
+                </p>
+                <Link
+                  to="/blog"
+                  className="text-primary underline text-base font-semibold"
+                >
+                  Read full article
+                </Link>
+              </div>
+              <img
+                src={blogTwo}
+                alt="blogTwo"
+                className="rounded-sm lg:w-full"
+              />
+            </div>
+          </section>
         </section>
       </div>
     </SectionWrapper>
--- src/organisms/SectionThree.js
+++ src/organisms/SectionThree.js
@@ -8,11 +8,11 @@ import { Link } from "react-router-dom";
 const SectionThree = () => {
   return (
     <div className="bg-gray pt-12 pb-8 lg:mt-12 lg:flex lg:flex-row-reverse lg:justify-between">
-      <div>
+      <div className="lg:mr-36 lg:mt-12">
         <Aside
           title="Meet Our Team"
           text="We’re a team of innovators with deep expertise in Human Resource, Product Design, Business Process and Tech, backed by some of the finest talents in the country."
-          className="lg:mt-16 lg:max-w-xs"
+          className="lg:mt-16 lg:max-w-md"
         />
         <Link to="/aboutus">
           <button class="bg-primary text-white text-sm font-bold py-4 px-8 rounded-md my-8 text-center">
@@ -38,7 +38,7 @@ const SectionThree = () => {
         </div> */}
       {/* </section> */}
 
-      <section className="mt-8 lg:mt-4">
+      <section className="mt-8 lg:mt-4 lg:ml-64 mb-16">
         <img src={founder} alt="founder" className="w-1/2 object-cover" />
         <img src={cto} alt="cto" className="w-1/2 object-cover mt-8 ml-20" />
         <img src={coo} alt="coo" className="w-1/2 object-cover mt-12" />
