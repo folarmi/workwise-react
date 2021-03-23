83d4937555125a88979c60b8c47b1903186172e1
folarmi <fola4ril@gmail.com>
2021-03-23 18:58:09 +0000

    added team

--- src/organisms/SectionThree.js
+++ src/organisms/SectionThree.js
@@ -1,9 +1,10 @@
 import { Aside } from "../molecules";
 import founder from "../assets/founder.png";
-import coo from "../assets/coo.png";
-// import founderTwo from "../assets/founderTwo.png";
+import ctoTwo from "../assets/ctoTwo.png";
+import founderTwo from "../assets/founderTwo.png";
 import cto from "../assets/cto.png";
 import { Link } from "react-router-dom";
+import TeamImage from "../templates/TeamImage";
 
 const SectionThree = () => {
   return (
@@ -21,27 +22,24 @@ const SectionThree = () => {
         </Link>
       </div>
 
-      {/* <section className="lg:flex"> */}
-      {/* <div class="flex flex-wrap justify-center">
-        <div class="w-6/12 sm:w-4/12 px-4 ">
-          <img
-            src={founderTwo}
-            alt="..."
-            className="shadow rounded-full max-w-full h-auto align-middle w-8/12 border border-primary"
-          />
-        </div>
-      </div> */}
-
-      {/* <div>
-          <p>Niyi Adisa</p>
-          <p>coo</p>
-        </div> */}
-      {/* </section> */}
-
-      <section className="mt-8 lg:mt-4 lg:ml-64 mb-16">
-        <img src={founder} alt="founder" className="w-1/2 object-cover" />
-        <img src={cto} alt="cto" className="w-1/2 object-cover mt-8 ml-20" />
-        <img src={coo} alt="coo" className="w-1/2 object-cover mt-12" />
+      <section className="mt-8 lg:mt-4 lg:ml-44 mb-16">
+        <TeamImage
+          image={founderTwo}
+          name="Niyi Adisa"
+          position="Founder/CEO"
+        />
+        <TeamImage
+          image={ctoTwo}
+          name="Ilyas Farawe"
+          position="Chief Technology Officer"
+          className="mt-8 ml-20"
+        />
+        <TeamImage
+          image={ctoTwo}
+          name="Temitope Ozreks"
+          position="Chief Operations Officer"
+          className="mt-12"
+        />
       </section>
     </div>
   );
